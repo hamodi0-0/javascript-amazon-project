18d6f0ab91e4107eb1d45f23220305e313658e3b
hamodi <mh9988527@gmail.com>
2024-10-30 15:03:28 +0300

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -75,7 +75,16 @@ document.querySelector('.js-products-grid')
            quantity: 1
           });
         }
-          console.log(cart)
+
+        let cartQuantity = 0;
+
+        cart.forEach((item)=>{
+          cartQuantity+=item.quantity
+        })
+
+        document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
         });
         
       });
