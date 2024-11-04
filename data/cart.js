0176134e9079af08cd9ff044256d06dc8cdcd4f5
hamodi <mh9988527@gmail.com>
2024-11-04 11:40:23 +0200

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [
+export let cart = JSON.parse(
+  localStorage.getItem('cart')
+) || [
 {
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
@@ -9,6 +11,10 @@ export let cart = [
   quantity:1
 }
 ];
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
 
 export function addToCart(productId){
 
@@ -31,12 +37,14 @@ export function addToCart(productId){
    });
  }
 
+ saveToStorage();
+
 }
 
 export function removeFromCart(productId){
 
     const newCart = [];
-    
+
     cart.forEach((cartItem)=>{
       if(cartItem.productId !== productId){
         newCart.push(cartItem);
@@ -44,4 +52,6 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
